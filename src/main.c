da41b22ad4197bad15ecdb2b4c63f1a8708b5402
Sven Herzberg <sven@imendio.com>
2007-08-19 16:52:33 +0200

    [src/main] added a window title and the default window size

--- src/main.c
+++ src/main.c
@@ -23,6 +23,8 @@
 
 #include <gtk/gtk.h>
 
+#include <glib/gi18n.h>
+
 int
 main (int   argc,
       char**argv)
@@ -30,7 +32,11 @@ main (int   argc,
 	GtkWidget* window;
 
 	gtk_init (&argc, &argv);
-	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	window = gtk_window_new     (GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_default_size (GTK_WINDOW (window),
+				     400, 300);
+	gtk_window_set_title        (GTK_WINDOW (window),
+				     _("Classify"));
 	g_signal_connect (window, "destroy",
 			  G_CALLBACK (gtk_main_quit), NULL);
 
