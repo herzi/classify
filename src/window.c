7766ba38ddf1c12188ddca224073f025266b4ebb
Sven Herzberg <sven@imendio.com>
2007-08-20 06:18:41 +0200

    [src/window] added a menubar to open the preferences

--- src/window.c
+++ src/window.c
@@ -24,6 +24,9 @@
 #include "window.h"
 
 #include <gtk/gtk.h>
+#include "preferences.h"
+
+#include <glib/gi18n.h>
 
 GtkWidget*
 c_window_get_vbox (CWindow* self)
@@ -31,15 +34,69 @@ c_window_get_vbox (CWindow* self)
 	return gtk_bin_get_child (GTK_BIN (self));
 }
 
+static void
+open_prefs (GtkAction* action,
+	    CWindow  * self)
+{
+	GtkWidget* dialog = c_preferences_new (GTK_WINDOW (self));
+	gtk_dialog_run     (GTK_DIALOG (dialog));
+	gtk_widget_destroy (dialog);
+}
+
 GtkWidget*
 c_window_new (void)
 {
-	GtkWidget* result = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-	GtkWidget* vbox   = gtk_vbox_new   (FALSE, 0);
+	GtkActionEntry  entries[] = {
+		{"File", NULL, N_("_File")},
 
-	gtk_widget_show   (vbox);
-	gtk_container_add (GTK_CONTAINER (result),
-			   vbox);
+		{"EditPreferences", GTK_STOCK_PREFERENCES, NULL,
+		 NULL, NULL, // FIXME: add tooltip
+		 G_CALLBACK (open_prefs)}
+	};
+	GtkActionGroup* group;
+	GtkUIManager* ui_manager = gtk_ui_manager_new ();
+	GtkWidget   * result = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	GtkWidget   * vbox   = gtk_vbox_new   (FALSE, 0);
+	GError      * error = NULL;
+
+	group = gtk_action_group_new ("main-group");
+	gtk_action_group_add_actions (group, entries, G_N_ELEMENTS (entries), result);
+
+	gtk_ui_manager_insert_action_group (ui_manager,
+					    group,
+					    0);
+	gtk_ui_manager_add_ui_from_string  (ui_manager,
+					    "<ui>"
+						"<menubar name='menubar'>"
+							"<menu action='File'>"
+								"<menuitem action='EditPreferences' />"
+							"</menu>"
+						"</menubar>"
+					    "</ui>",
+					    -1,
+					    &error);
+	g_object_unref (group);
+
+	if (error) {
+		g_warning ("Error setting up the user interface: %s",
+			   error->message);
+		g_error_free (error);
+		error = NULL;
+	}
+
+	gtk_box_pack_start (GTK_BOX (vbox),
+			    gtk_ui_manager_get_widget (ui_manager, "/menubar"),
+			    FALSE,
+			    FALSE,
+			    0);
+	gtk_widget_show    (vbox);
+	gtk_container_add  (GTK_CONTAINER (result),
+			    vbox);
+
+	g_object_set_data_full (G_OBJECT (result),
+				"CWindow::UIManager",
+				ui_manager,
+				g_object_unref);
 
 	return result;
 }
