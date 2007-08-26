0cf44e115dfb961dd07ba3c5dae89f833eb6aaad
Sven Herzberg <sven@imendio.com>
2007-08-26 21:28:06 +0200

    [src/task] dropped the constructor

--- src/task.c
+++ src/task.c
@@ -46,19 +46,6 @@ c_task_init (CTask* self)
 						      CTaskPrivate);
 }
 
-static GObject*
-task_constructor (GType                  type,
-		  guint                  n_params,
-		  GObjectConstructParam* params)
-{
-	GObject* result = G_OBJECT_CLASS(c_task_parent_class)->constructor (type,
-									    n_params,
-									    params);
-	CTask  * self   = C_TASK (result);
-
-	return result;
-}
-
 static void
 task_finalize (GObject* object)
 {
@@ -131,7 +118,6 @@ c_task_class_init (CTaskClass* self_class)
 {
 	GObjectClass* object_class = G_OBJECT_CLASS (self_class);
 
-	object_class->constructor  = task_constructor;
 	object_class->finalize     = task_finalize;
 	object_class->get_property = task_get_property;
 	object_class->set_property = task_set_property;
